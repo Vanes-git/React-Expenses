bc96a2eb00f18c0bb7b645a4bf631d2e885f9f0d
Vanes <vanes.sysoev@gmail.com>
2021-05-14 17:28:37 +0300

    feat: editMode for newExpenses

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [editMode, setEditMode] = useState(false);
+
   const onSaveExpenseDataHandler = (enteredExpenseData) => {
-    const expenseData = { 
+    const expenseData = {
       ...enteredExpenseData,
-       id: Math.random().toString() 
+      id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setEditMode(false);
+  };
+
+  const toggleEditMode = () => {
+    setEditMode((prevMode) => {
+      return !prevMode;
+    });
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler} />
+      {editMode === true ? (
+        <ExpenseForm
+          onSaveExpenseData={onSaveExpenseDataHandler}
+          onCancel={toggleEditMode}
+        />
+      ) : (
+        <button onClick={toggleEditMode}>Add New Expense</button>
+      )}
     </div>
   );
 };
