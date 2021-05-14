842e00ccf54d14a1e0a75a6895eab0708f37a4e0
Vanes <vanes.sysoev@gmail.com>
2021-05-14 18:22:22 +0300

    feat: CharBars

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ExpensesList from "./ExpensesList";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesChart from './ExpensesChart';
 import Card from "../UI/Card";
 import "./Expenses.css";
 
@@ -22,6 +23,7 @@ const Expenses = (props) => {
         selectedYear={selectedYear}
         onChangeYear={yearChangeHandler}
       />
+      <ExpensesChart expenses = {visibleData} />
       <ExpensesList expenses = {visibleData}/>
     </Card>
   );
